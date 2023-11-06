d45b183c3a45da53d1a5f61f671c31cd7d1994c5
Yard0 <1997yaroslavnesenchuk1997@gmail.com>
2023-11-06 18:38:57 +0200

    Created new branch with empty project only

--- APM-Final-v16/src/app/home/welcome.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    templateUrl: './welcome.component.html',
-    standalone: true
-})
-export class WelcomeComponent {
-  public pageTitle = 'Welcome';
-}
