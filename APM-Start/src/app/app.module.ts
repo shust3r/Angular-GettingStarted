6b2ad1b235c7f4a4f872cd3a253b7d395a89a74e
Yard0 <1997yaroslavnesenchuk1997@gmail.com>
2023-11-10 16:10:51 +0200

    The "Shared" module was added

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,45 +1,29 @@
-//app module - root app module, which declares app components
-//with "import" statement and "declarations"
+//app module - root app module, which declares app components with "import" statement and "declarations"
 import { NgModule } from '@angular/core';
 //BrowserModule - for the features that needs to run this app in a browser
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 //AppCompon
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent, //AppComponent - the first component, loaded for the app
-    WelcomeComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: "products", component: ProductListComponent },
-      {
-        path: "products/:id",
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent
-      },
       { path: "welcome", component: WelcomeComponent },
       { path: "", redirectTo: "welcome", pathMatch: "full" },
       { path: "**", redirectTo: "welcome", pathMatch: "full" }
-    ])
+    ]),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
